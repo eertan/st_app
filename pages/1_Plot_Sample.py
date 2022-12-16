8dbd2def6014e52f4d74c304a90801705dd7bca8
eertan <erolertan4@gmail.com>
2022-12-16 15:17:01 -0500

    Finished scikit example, should find a way to cache

--- pages/1_Plot_Sample.py
+++ pages/1_Plot_Sample.py
@@ -5,6 +5,10 @@ from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
 from sklearn.compose import make_column_selector
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+import numpy as np
+from sklearn.model_selection import cross_validate
+import matplotlib.pyplot as plt
 
 
 @st.experimental_memo
@@ -55,7 +59,7 @@ st.write(f"Number of features: {X.shape[1]}")
 st.write(f"Number of categorical features: {n_categorical_features}")
 st.write(f"Number of numerical features: {n_numerical_features}")
 
-st.markdown('### Baseline with dropped categories')
+st.markdown('### Gradient Boosting on Ames Housing: Dropped, one-hot, ordinal and native ordinal comparison')
 
 
 dropper = make_column_transformer(
@@ -63,6 +67,105 @@ dropper = make_column_transformer(
 )
 hist_dropped = make_pipeline(dropper, HistGradientBoostingRegressor(random_state=42))
 
-st.markdown('### One-hot encoding')
+one_hot_encoder = make_column_transformer(
+    (
+        OneHotEncoder(sparse_output=False, handle_unknown="ignore"),
+        make_column_selector(dtype_include="category"),
+    ),
+    remainder="passthrough",
+)
+
+hist_one_hot = make_pipeline(
+    one_hot_encoder, HistGradientBoostingRegressor(random_state=42)
+)
+
+ordinal_encoder = make_column_transformer(
+    (
+        OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=np.nan),
+        make_column_selector(dtype_include="category"),
+    ),
+    remainder="passthrough",
+    # Use short feature names to make it easier to specify the categorical
+    # variables in the HistGradientBoostingRegressor in the next step
+    # of the pipeline.
+    verbose_feature_names_out=False,
+)
+
+hist_ordinal = make_pipeline(
+    ordinal_encoder, HistGradientBoostingRegressor(random_state=42)
+)
+
+hist_native = make_pipeline(
+    ordinal_encoder,
+    HistGradientBoostingRegressor(
+        random_state=42,
+        categorical_features=categorical_columns,
+    ),
+).set_output(transform="pandas")
+
+scoring = "neg_mean_absolute_percentage_error"
+n_cv_folds = 3
+
+dropped_result = cross_validate(hist_dropped, X, y, cv=n_cv_folds, scoring=scoring)
+one_hot_result = cross_validate(hist_one_hot, X, y, cv=n_cv_folds, scoring=scoring)
+ordinal_result = cross_validate(hist_ordinal, X, y, cv=n_cv_folds, scoring=scoring)
+native_result = cross_validate(hist_native, X, y, cv=n_cv_folds, scoring=scoring)
 
 
+def plot_results(figure_title):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+
+    plot_info = [
+        ("fit_time", "Fit times (s)", ax1, None),
+        ("test_score", "Mean Absolute Percentage Error", ax2, None),
+    ]
+
+    x, width = np.arange(4), 0.9
+    for key, title, ax, y_limit in plot_info:
+        items = [
+            dropped_result[key],
+            one_hot_result[key],
+            ordinal_result[key],
+            native_result[key],
+        ]
+
+        mape_cv_mean = [np.mean(np.abs(item)) for item in items]
+        mape_cv_std = [np.std(item) for item in items]
+
+        ax.bar(
+            x=x,
+            height=mape_cv_mean,
+            width=width,
+            yerr=mape_cv_std,
+            color=["C0", "C1", "C2", "C3"],
+        )
+        ax.set(
+            xlabel="Model",
+            title=title,
+            xticks=x,
+            xticklabels=["Dropped", "One Hot", "Ordinal", "Native"],
+            ylim=y_limit,
+        )
+    fig.suptitle(figure_title)
+    return fig
+
+
+fig1 = plot_results("Gradient Boosting on Ames Housing")
+
+st.pyplot(fig1)
+ 
+
+st.markdown("### limiting splits to 3 comparison")
+
+for pipe in (hist_dropped, hist_one_hot, hist_ordinal, hist_native):
+    pipe.set_params(
+        histgradientboostingregressor__max_depth=3,
+        histgradientboostingregressor__max_iter=15,
+    )
+
+dropped_result = cross_validate(hist_dropped, X, y, cv=n_cv_folds, scoring=scoring)
+one_hot_result = cross_validate(hist_one_hot, X, y, cv=n_cv_folds, scoring=scoring)
+ordinal_result = cross_validate(hist_ordinal, X, y, cv=n_cv_folds, scoring=scoring)
+native_result = cross_validate(hist_native, X, y, cv=n_cv_folds, scoring=scoring)
+
+st.pyplot(plot_results("Gradient Boosting on Ames Housing (few and small trees)"))
