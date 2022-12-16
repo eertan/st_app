8a556ce6a84dacf5ca5c0b26bfbcf77c9c909c56
eertan <erolertan4@gmail.com>
2022-12-16 12:15:42 -0500

    repeat scikit categorical

--- pages/1_Plot_Sample.py
+++ pages/1_Plot_Sample.py
@@ -1,4 +1,68 @@
+from sklearn.datasets import fetch_openml
+import streamlit as st
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.compose import make_column_selector
+from sklearn.preprocessing import OneHotEncoder
 
-import dask.dataframe as ds
 
- # ds.read_csv(<file>)
+@st.experimental_memo
+def fetch_data():
+    return fetch_openml(data_id=42165, as_frame=True,
+                        return_X_y=True, parser="pandas")
+
+
+# Select only a subset of features of X to make the example faster to run
+categorical_columns_subset = [
+    "BldgType",
+    "GarageFinish",
+    "LotConfig",
+    "Functional",
+    "MasVnrType",
+    "HouseStyle",
+    "FireplaceQu",
+    "ExterCond",
+    "ExterQual",
+    "PoolQC",
+]
+
+
+numerical_columns_subset = [
+    "3SsnPorch",
+    "Fireplaces",
+    "BsmtHalfBath",
+    "HalfBath",
+    "GarageCars",
+    "TotRmsAbvGrd",
+    "BsmtFinSF1",
+    "BsmtFinSF2",
+    "GrLivArea",
+    "ScreenPorch",
+]
+
+
+X, y = fetch_data()
+X = X[categorical_columns_subset + numerical_columns_subset]
+X[categorical_columns_subset] = X[categorical_columns_subset].astype("category")
+
+categorical_columns = X.select_dtypes(include="category").columns
+n_categorical_features = len(categorical_columns)
+n_numerical_features = X.select_dtypes(include="number").shape[1]
+
+st.write(f"Number of samples: {X.shape[0]}")
+st.write(f"Number of features: {X.shape[1]}")
+st.write(f"Number of categorical features: {n_categorical_features}")
+st.write(f"Number of numerical features: {n_numerical_features}")
+
+st.markdown('### Baseline with dropped categories')
+
+
+dropper = make_column_transformer(
+    ("drop", make_column_selector(dtype_include="category")), remainder="passthrough"
+)
+hist_dropped = make_pipeline(dropper, HistGradientBoostingRegressor(random_state=42))
+
+st.markdown('### One-hot encoding')
+
+
